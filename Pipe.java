c8b4a44d1866691eca2f80947b582d9e421710dc
mmgbeojiri <michaelm1464@nycstudents.net>
2026-02-13 08:56:47 -0500

    udpate 3:

--- Pipe.java
+++ Pipe.java
@@ -2,13 +2,13 @@ import java.awt.*;
 import javax.swing.*;
 public class Pipe {
 
-  Image botimage;
-  Image topimage;
+  Image capImage;
+  Image lineImage;
   
   Pipe(){
 
-    this.botimage = new ImageIcon("./JavaLessonGraphicsLesson-7/images/pipe_bot.png").getImage();
-    this.topimage = new ImageIcon("./JavaLessonGraphicsLesson-7/images/pipe_top.png").getImage();
+    this.capImage = new ImageIcon("./JavaLessonGraphicsLesson-7/images/pipe_cap.png").getImage();
+    this.lineImage = new ImageIcon("./JavaLessonGraphicsLesson-7/images/pipe_line.png").getImage();
 
 
   }
@@ -17,30 +17,44 @@ public class Pipe {
 
    
     
-    Game.canvas.drawImage(this.botimage,
-                          Globals.coinX,
-                          Globals.coinY + 200 - 100,
-                          52, //img width
-                          270,//img height
+    Game.canvas.drawImage(this.capImage,
+                          Globals.coinX-22, //22 is the width of the pipe minus width of coin
+                          Globals.coinY + 100,
+                          52*2, //img width
+                          24*2,//img height
                           null);   
                             
                           
                           
-        Game.canvas.drawImage(this.topimage,
-                          Globals.coinX,
-                          Globals.coinY - 200 - 100,
-                          52, //img width
-                          270,//img height
+        Game.canvas.drawImage(this.capImage,
+                          Globals.coinX-22,
+                          Globals.coinY - 100,
+                          52*2, //img width
+                          24*2,//img height
                           null); 
                         
                         
-                        }
-
                           
+                          Game.canvas.drawImage(this.lineImage,
+                            Globals.coinX-18, //22 is the width of the pipe minus width of coin
+                            Globals.coinY + 100+48,
+                            48*2, //img width
+                            Game.height,//img height
+                            null); 
 
-
-
-
+                            Game.canvas.drawImage(this.lineImage,
+                            Globals.coinX-18, //22 is the width of the pipe minus width of coin
+                            Globals.coinY - 780,
+                            48*2, //img width
+                            Game.height,//img height
+                            null); 
+                            
+                            
+                            
+                            
+                            
+                            
+                          }
 
     }
 
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ press click
 
 # Update 2: Added Non-collidable Pipes
 ![pipes](image-1.png)
+
+# Update 3: Added Good Looking Pipes
+![long pipes](image-2.png)
